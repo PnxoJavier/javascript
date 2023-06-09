8d891d0317b087fec9359bd663ccebe7bfa83015
PnxoJavier <128553481+PnxoJavier@users.noreply.github.com>
2023-06-09 03:42:29 -0400

    MODIFICACIONES DOM
    MODIFICACIONES EN DOM

--- script.js
+++ script.js
@@ -1,59 +1,104 @@
 
-//incio solicita el nombre para interactuar
-
-let nombre =prompt("¿Cual es tu nombre").toLowerCase();
-
-alert("Hola " + nombre + ". Esta es una calculadora de Impuestos Aduaneros Chile");
-
-//prompt el fin es poder determinar si coloca la frase Licores o Cigarros, el sistema podria agregar el 15% como adicional
-
-let tipodeproducto =prompt ("Indica que tipo de producto vas a importar. El Licores y cigarros tienen un 15% de impuesto adicional").toLocaleLowerCase();
-
-// Prompt para que indique el valor del producto a importar
-
-let valor = parseInt(prompt("Cuál es valor de tu producto"));
-
-// Se establecio como Objeto para almacenar los impuestos y porcentajes
-
-    let impuestos = {
-        arancel:    0,
-        iva : 0,
-        adicional : 0,
-        };
+//Funcion Para realizar el Local Storage 
+function test(){
+var nombre = document.getElementById("nombres").value;
+var email = document.getElementById("mail").value;
 
 
-// Array de productos exentos de impuestos
-let productosExentos = ["libro", "medicamento", "alimento"];
+//Guardar en el local storage y posterior llamrlo en el HTML
 
-// Array de productos con impuesto adicional del 15%
-let productosConImpuestoAdicional = ["licores", "cigarros"];
+var user = localStorage.setItem("nombre", nombre);
+var mailto = localStorage.setItem("email", email);
 
-    // Verificar si el producto está exento de impuestos
-    if (valor >= 47 && !productosExentos.includes(tipodeproducto.toLowerCase())) {
-    impuestos.iva = valor * 0.19;
-
-    // Verificar si el producto tiene impuesto adicional del 15%
-    if (productosConImpuestoAdicional.includes(tipodeproducto.toLowerCase())) {
-        impuestos.adicional = valor * 0.15;
-    } else {
-        impuestos.arancel= valor * 0.06;
-    }
 }
-// Se crea un Alert para que si el valor total es superior a 1000 dolares indique que tome un contacto con un agente de aduanas 
-    let valortotal = (valor + impuestos.iva + impuestos.adicional + impuestos.arancel)
+// Remover informacion del Local Storage
+function remove(){
+localStorage.removeItem("nombre");
+localStorage.removeItem("email");
 
-        alert (`El valor de tu impuesto es ${impuestos.arancel} , el valor de tu IVA es ${impuestos.iva} , si es Licor o cigarro su impuesto es de ${impuestos.adicional} por lo cual el valor total es  ${valortotal} dolares`);
+}
 
-    function Validaragente(valortotal) {
-        
-        if (valortotal > 1000) {
-            alert(`Tome Contacto con Agente Aduanero el producto con el valor de ${valortotal} es superior a 1.000 dolares`)
-                    
-        } else {
-            alert (`Por el valor del producto, no debe contactar a un agente aduanbero`)
+// Falta Mostrar  "nombre" del Local Storage en el HTML 
 
-                    
-        }
-
+function mostrarNombre() {
+    var nombre = localStorage.getItem("nombre");
+    if (nombre) {
+      document.getElementById("nombre-guardado").textContent = nombre;
     }
-    Validaragente(valortotal);
+  }
+
+//El fin es poder determinar si coloca la frase Licores o Cigarros, el sistema podria agregar el 15% como adicional
+
+
+//Cargar productos 
+
+//MANTENGO PROBLEMAS CON EL ITEM DE CARGAR PRODUCTOS PARA VERLOS EN EL HTML
+
+function cargarProductos() {
+    var seleccionarProd = document.getElementById("seleccionarprod").value;
+  
+   const productosJSON = {
+    "productosgeneral": ["Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles", "Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"]
+    };
+  
+const tipoProducto = productosJSON[seleccionarProd];
+  
+    // Obtener el elemento select del HTML
+ var productoSelect = document.getElementById("product");
+ 
+ //ME MARCA ERROR EN LA LINEA 49
+ productoSelect.innerHTML = "<option value=''> 'Seleccione el producto a importar' </option>";
+  
+    // Agregar opciones al elemento select
+    for (var i = 0; i < tipoProducto.length; i++) {
+     var option = document.createElement("option");
+     option.value = tipoProducto[i];
+     option.textContent = tipoProducto[i];
+    productoSelect.appendChild(option);
+   }
+ }
+
+// Calcular impuestos y valor total
+function calcularImpuestos() {
+    var valor = parseFloat(document.getElementById("valores").value);
+    var producto = document.getElementById("product").value.toLowerCase();
+  
+    var impuestos = {
+      arancel: 0,
+      iva: 0,
+      adicional: 0
+    };
+    
+    var productossinimpuesto = ["Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles"];
+    var productosconimpuestos = ["Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"];
+  
+    if (valor >= 47 && !productossinimpuesto.includes(producto)) {
+      impuestos.iva = valor * 0.19;
+  
+      if (productosconimpuestos.includes(producto)) {
+        impuestos.adicional = valor * 0.15;
+      } else {
+        impuestos.arancel = valor * 0.06;
+      }
+    }
+
+
+    // Calcular el valor total incluyendo impuestos
+    let valorTotal = valores + impuestos.iva + impuestos.adicional + impuestos.arancel;
+
+    // Mostrar el resultado al usuario
+    alert(`El valor de tu impuesto es ${impuestos.arancel}, el valor de tu IVA es ${impuestos.iva}. Si es licor o cigarro, el impuesto adicional es ${impuestos.adicional}. El valor total es ${valorTotal} dólares.`);
+
+    // Función para validar si se debe contactar a un agente aduanero si el valor total es superior a 1000 dólares
+    function validarAgente(valorTotal) {
+        if (valorTotal > 1000) {
+        alert(`Contacta con un agente aduanero. El valor del producto (${valorTotal} dólares) es superior a 1000 dólares.`);
+        } else {
+        alert("No es necesario contactar a un agente aduanero por el valor del producto.");
+        }
+    }
+
+    validarAgente(valorTotal);
+}
+
+
