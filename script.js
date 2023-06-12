22cbd70c378f69fe08d62a395b777dcdf9dff9ee
PnxoJavier <128553481+PnxoJavier@users.noreply.github.com>
2023-06-11 22:47:31 -0400

    crear nueva pagina
    se crea para hacer los callculos

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 //Funcion Para realizar el Local Storage 
 function test(){
-var nombre = document.getElementById("nombres").value;
-var email = document.getElementById("mail").value;
+const nombre = document.getElementById("nombres").value;
+const email = document.getElementById("mail").value;
 
 
 //Guardar en el local storage y posterior llamrlo en el HTML
 
-var user = localStorage.setItem("nombre", nombre);
-var mailto = localStorage.setItem("email", email);
+const user = localStorage.setItem("nombre", nombre);
+const mailto = localStorage.setItem("email", email);
 
 }
 // Remover informacion del Local Storage
@@ -32,45 +32,42 @@ function mostrarNombre() {
 
 //Cargar productos 
 
-//MANTENGO PROBLEMAS CON EL ITEM DE CARGAR PRODUCTOS PARA VERLOS EN EL HTML
-
 function cargarProductos() {
-    var seleccionarProd = document.getElementById("seleccionarprod").value;
+    const seleccionarProd = document.getElementById("seleccionarprod").value;
   
-   const productosJSON = {
-    "productosgeneral": ["Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles", "Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"]
-    };
+   const productosJSON = [
+    "Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles", "Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"
+  ] ;
   
 const tipoProducto = productosJSON[seleccionarProd];
   
     // Obtener el elemento select del HTML
- var productoSelect = document.getElementById("product");
+ const productoSelect = document.getElementById("seleccionarprod");
  
- //ME MARCA ERROR EN LA LINEA 49
- productoSelect.innerHTML = "<option value=''> 'Seleccione el producto a importar' </option>";
+ productoSelect.HTMLOptionElement = "<option value=''> 'Seleccione el producto a importar' </option>";
   
     // Agregar opciones al elemento select
-    for (var i = 0; i < tipoProducto.length; i++) {
+    for (var i = 0; i <productosJSON.length; i++) {
      var option = document.createElement("option");
-     option.value = tipoProducto[i];
-     option.textContent = tipoProducto[i];
+     option.value = productosJSON[i];
+     option.textContent = productosJSON[i];
     productoSelect.appendChild(option);
    }
  }
 
 // Calcular impuestos y valor total
 function calcularImpuestos() {
-    var valor = parseFloat(document.getElementById("valores").value);
-    var producto = document.getElementById("product").value.toLowerCase();
+    const valor = parseFloat(document.getElementById("valores").value);
+    const producto = (document.getElementById("producto").value);
   
-    var impuestos = {
+    const impuestos = {
       arancel: 0,
       iva: 0,
       adicional: 0
     };
-    
-    var productossinimpuesto = ["Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles"];
-    var productosconimpuestos = ["Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"];
+    //El fin es poder determinar si coloca la frase Licores o Cigarros, el sistema podria agregar el 15% como adicional
+    const productossinimpuesto = ["Libros", "Electrodomesticos", "Cuadernos", "Utileria", "Comida", "Muebles"];
+    const productosconimpuestos = ["Vino", "Whiskey", "Pisco", "Fernet", "Malboro", "Lucky", "Kent", "Dunhill", "Pall Mall"];
   
     if (valor >= 47 && !productossinimpuesto.includes(producto)) {
       impuestos.iva = valor * 0.19;
@@ -84,21 +81,54 @@ function calcularImpuestos() {
 
 
     // Calcular el valor total incluyendo impuestos
-    let valorTotal = valores + impuestos.iva + impuestos.adicional + impuestos.arancel;
+    let valorTotal = valor + impuestos.iva + impuestos.adicional + impuestos.arancel;
 
     // Mostrar el resultado al usuario
-    alert(`El valor de tu impuesto es ${impuestos.arancel}, el valor de tu IVA es ${impuestos.iva}. Si es licor o cigarro, el impuesto adicional es ${impuestos.adicional}. El valor total es ${valorTotal} dólares.`);
+    // alert(`El valor de tu impuesto es ${impuestos.arancel}, el valor de tu IVA es ${impuestos.iva}. Si es licor o cigarro, el impuesto adicional es ${impuestos.adicional}. El valor total es ${valorTotal} dólares.`);
+
+    const resultadoElement = document.getElementById("msj");
+    resultadoElement.textContent = `El valor de tu impuesto es ${impuestos.arancel}, el valor de tu IVA es ${impuestos.iva}. Si es licor o cigarro, el impuesto adicional es ${impuestos.adicional}. El valor total es ${valorTotal} dólares.`;
+
+
+
 
     // Función para validar si se debe contactar a un agente aduanero si el valor total es superior a 1000 dólares
     function validarAgente(valorTotal) {
         if (valorTotal > 1000) {
-        alert(`Contacta con un agente aduanero. El valor del producto (${valorTotal} dólares) es superior a 1000 dólares.`);
+          const resultadoElement = document.getElementById("agente");
+          resultadoElement.textContent = `Contacta con un agente aduanero. El valor del producto (${valorTotal} dólares) es superior a 1000 dólares.`;
         } else {
-        alert("No es necesario contactar a un agente aduanero por el valor del producto.");
+          const resultadoElement = document.getElementById("agente");
+          resultadoElement.textContent = "No es necesario contactar a un agente aduanero por el valor del producto.";
         }
+    
+      }
+
+      // Mostrar el resultado al usuario
+      
+
+ 
+       validarAgente(valorTotal);
+
+
     }
 
-    validarAgente(valorTotal);
-}
+
+
+
+//generar  un cuadro con los valores, para dejar de utilizar el Alert.
+
+
+
+
+
+       
+    
+    
+ 
+
+
+   
+
 
 
